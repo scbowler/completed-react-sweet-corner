92c419a8d83737b501abc766b4c1325c28a78dbb
Scott Bowler <scott.bowler@learningfuze.com>
2019-04-26 16:29:49 -0700

    Feature set 1 complete

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -4,7 +4,7 @@ import './about.scss';
 export default props => {
     return (
         <div>
-            <h1>About Page</h1>
+            <h1 className="center">About Page</h1>
         </div>
     );
 }
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import '../assets/css/app.scss';
+import About from './about';
+import Contact from './contact';
+import Footer from './footer';
+import Header from './header';
+import Home from './home';
+import Services from './services';
 
 const App = () => (
-    <div>
-        <h1>Sweet Corner</h1>
+    <div className="app">
+        <div className="container">
+            <Header />
+
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/" exact component={Home} />
+            <Route path="/services" component={Services} />
+            
+            <Footer />
+        </div>
     </div>
 );
 
