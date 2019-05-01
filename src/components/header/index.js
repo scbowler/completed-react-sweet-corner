50ad5500106aee17ab046f8f10a1777522ae99b1
Scott Bowler <scott.bowler@learningfuze.com>
2019-04-30 17:58:50 -0700

    Header design in progress

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Nav from '../nav';
 import './header.scss';
 
 export default props => {
     return (
-        <div>
-            <h1 className="center">This is the Header</h1>
+        <div className="header">
+            <div className="header-awning"/>
+            
+            <Nav/>
+
+            <div className="header-logo"/>
+            <h1 className="center fancy">We deliver cupcakes for the important events in your life!</h1>
+            <div className="header-bottom-bar"/>
         </div>
     );
 }
