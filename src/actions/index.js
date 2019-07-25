3f28ecaa5b41eba9c4f0788a59238b9bc032adbc
Scott Bowler <scott.bowler@learningfuze.com>
2019-07-25 13:19:35 -0700

    Initial cart styling done

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,16 +6,9 @@ const BASE_URL = 'http://api.sc.lfzprototypes.com';
 
 export const addItemToCart = (productId, quantity) => async (dispatch) => {
     try {
-        const cartToken = localStorage.getItem('sc-cart-token');
-        const axiosConfig = {
-            headers: {
-                'X-Cart-Token': cartToken
-            }
-        };
-
         const resp = await axios.post(`${BASE_URL}/api/cart/items/${productId}`, {
             quantity: quantity
-        }, axiosConfig);
+        }, withHeaders());
 
         localStorage.setItem('sc-cart-token', resp.data.cartToken);
 
@@ -32,14 +25,7 @@ export const clearProductDetails = () => ({ type: types.CLEAR_PRODUCT_DETAILS })
 
 export const getActiveCart = () => async dispatch => {
     try {
-        const cartToken = localStorage.getItem('sc-cart-token');
-        const axiosConfig = {
-            headers: {
-                'X-Cart-Token': cartToken
-            }
-        };
-
-        const resp = await axios.get(`${BASE_URL}/api/cart`, axiosConfig);
+        const resp = await axios.get(`${BASE_URL}/api/cart`, withHeaders());
 
         dispatch({
             type: types.GET_ACTIVE_CART,
@@ -49,17 +35,6 @@ export const getActiveCart = () => async dispatch => {
         console.log('Get active cart error:', error);
     }
 }
-
-// export const getCart = () => async dispatch => {
-//     try {
-//         const resp = await axios.get(BASE_URL + '/api/cart', withHeaders());
-
-//         console.log('Get Cart Resp:', resp);
-
-//     } catch(err){
-//         console.log('Error getting cart:', err);
-//     }
-// }
 
 export const getAllProducts = () => async dispatch => {
     try {
@@ -76,14 +51,7 @@ export const getAllProducts = () => async dispatch => {
 
 export const getCartTotals = () => async dispatch => {
     try {
-        const cartToken = localStorage.getItem('sc-cart-token');
-        const axiosConfig = {
-            headers: {
-                'x-cart-token': cartToken
-            }
-        }
-
-        const resp = await axios.get(`${BASE_URL}/api/cart/totals`, axiosConfig);
+        const resp = await axios.get(`${BASE_URL}/api/cart/totals`, withHeaders());
 
         dispatch({
             type: types.GET_CART_TOTALS,
@@ -106,33 +74,3 @@ export const getProductDetails = productId => async dispatch => {
         console.log('Error getting product details:', err);
     }
 }
-
-// export const getAllProducts = () => async dispatch => {
-//     try {
-//         const { data: { products } } = await axios.get(BASE_URL + '/api/products');
-
-//         dispatch({
-//             type: types.GET_ALL_PRODUCTS,
-//             products: products
-//         });
-//     } catch(err){
-//         console.log('Get product list error:', err);
-
-//         // TODO: dispatch an error for failed products request
-//     }
-// }
-
-// export const getProductDetails = (productId) => async (dispatch) => {
-//     try {
-//         const { data: { product } } = await axios.get(`${BASE_URL}/api/products/${productId}`);
-
-//         dispatch({
-//             type: types.GET_PRODUCT_DETAILS,
-//             product: product
-//         });
-//     } catch(err){
-//         console.log('Error Getting product details:', err);
-
-//         // TODO: dispatch an error for failed product details request
-//     }
-// }
--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getActiveCart } from '../../actions';
+import Money from '../general/money';
 import './cart.scss';
 
 class Cart extends Component {
@@ -8,12 +9,75 @@ class Cart extends Component {
         this.props.getActiveCart();
     }
 
+    goToProduct(productId){
+        this.props.history.push(`/products/${productId}`);
+    }
+
+    renderItem = ({each, itemId, name, productId, quantity, thumbnail, total}) => {
+        return (
+            <tr className="cart-item" key={itemId} onClick={() => this.goToProduct(productId)}>
+                <td className="product-image">
+                    <img src={thumbnail.url} alt={thumbnail.altText}/>
+                </td>
+                <td className="product-name">{name}</td>
+                <td className="product-cost">
+                    <Money>{each}</Money>
+                </td>
+                <td className="product-quantity">{quantity}</td>
+                <td className="product-total">
+                    <Money>{total}</Money>
+                </td>
+            </tr>
+        );
+    }
+
+    renderMessageRow(message){
+        return (
+            <tr>
+                <td className="center" colSpan="5">{message}</td>
+            </tr>
+        );
+    }
+
     render(){
-        console.log('Cart Items:', this.props.cartItems);
+        const { cartItems, totals } = this.props;
+        const totalCost = totals?.cost || 0;
+        const totalItems = totals?.items || 0;
+        let itemElements = this.renderMessageRow('Loading Cart items');
+
+        if(cartItems){
+            if(cartItems.length){
+                itemElements = cartItems.map(this.renderItem);
+            } else {
+                itemElements = this.renderMessageRow('Cart Empty')
+            }
+        }
 
         return (
-            <div>
+            <div className="cart">
                 <h1 className="center">Cart</h1>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th className="header-product">Product</th>
+                            <th>Each</th>
+                            <th>quantity</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {itemElements}
+                        <tr className="cart-totals">
+                            <td colSpan="3">Cart Totals:</td>
+                            <td>{totalItems}</td>
+                            <td>
+                                <Money>{totalCost}</Money>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
@@ -21,7 +85,8 @@ class Cart extends Component {
 
 function mapStateToProps(state){
     return {
-        cartItems: state.cart.items
+        cartItems: state.cart.items,
+        totals: state.cart.total
     }
 }
 
